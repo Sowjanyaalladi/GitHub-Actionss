064188260438396c67b0979cc2cd95ade67dc448
Alladi Venkata Sowjanya <sowjialladi@gmail.com>
2026-06-26 12:59:37 +0530

    Change AMI ID for AWS EC2 instance
    Updated the AMI ID for the AWS instance resource.

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316"   # Amazon Linux 2 AMI (us-east-1)
+  ami           = "ami-08f44e8eca9095668"   # Amazon Linux 2 AMI (us-east-1)
   instance_type = "t2.micro"
-  key_name      = "your-key-pair-name"
+  
 
   tags = {
     Name = "MyEC2Instance"
