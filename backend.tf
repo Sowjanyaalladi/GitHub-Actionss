04a9b5cdfaf27001a8da62f099c0784f22500e76
Alladi Venkata Sowjanya <sowjialladi@gmail.com>
2026-06-26 13:37:45 +0530

    Add Terraform configuration for AWS S3 backend

---
+++ backend.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "sowjanya-terraform-state-bucket-12345"
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
