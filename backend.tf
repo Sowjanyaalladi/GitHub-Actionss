745aaaed77b7f7f7e7c469de6d6aaf1e1412c146
Alladi Venkata Sowjanya <sowjialladi@gmail.com>
2026-06-26 13:45:53 +0530

    Update S3 bucket name for Terraform state

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "sowjanya-terraform-state-bucket-12345"
+  bucket = "sowjanya-terraform-state-bucket-12344445"
 
   tags = {
     Name = "Terraform State Bucket"
