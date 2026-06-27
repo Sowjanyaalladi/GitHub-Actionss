6d7b4fbeeeb84e08274c8461b22cb32209dbc7a6
Alladi Venkata Sowjanya <sowjialladi@gmail.com>
2026-06-27 22:21:11 +0530

    Rename aws_instance resource to 'sowjiii'

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "name" {
+resource "aws_instance" "sowjiii" {
     ami           = "ami-08f44e8eca9095668"
     instance_type = "t2.micro"
     tags = {
-        Name = sowjiii
+        Name = "sowjiii"
     }
 }
 
