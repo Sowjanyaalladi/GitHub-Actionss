c3602e01f27d1db0d6d4b6d6b04389c06a1f6819
Alladi Venkata Sowjanya <sowjialladi@gmail.com>
2026-06-27 22:37:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "sowjiii" {
     ami           = "ami-08f44e8eca9095668"
     instance_type = "t3.micro"
+    instance_id   = "i-02eb9f4502dda07da"
     tags = {
         Name = "sowjiii"
     }
